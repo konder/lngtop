bf785d2c8c7c15d07b553effee98c464235976f6
zhangnan <konders@gmail.com>
2015-12-05 12:53:19 +0800

    Update init.sh

--- devops/init.sh
+++ devops/init.sh
@@ -69,8 +69,6 @@ mkdir /data
 mount  /dev/vda2 /data
 echo "/dev/vda2 /data ext4 defaults 0 0" >> /etc/fstab
 
-sed -i 's/openstacklocal/openstacklocal\nnameserver 172.16.100.111/' /etc/resolv.conf
-
 cat >> /etc/security/limits.conf <<EOF
 * soft nofile 32768
 * hard nofile 65536
